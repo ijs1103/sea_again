8c99145901c65c27476485d98a2c7cce3e112742
lee <ajaijs1103@naver.com>
2022-08-09 19:55:50 +0900

    fix: SearchResult 컴포넌트 애니메이션 적용

--- components/layout/SearchResult.tsx
+++ components/layout/SearchResult.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { cls } from '@utils/index'
 interface Props {
 	keyword: string
 }
 function SearchResult({ keyword }: Props) {
+	const [isClicked, setIsCliked] = useState(false)
 	return (
-		<div className='absolute top-4 left-1/2 -translate-x-1/2  bg-white shadow-2xl rounded-2xl p-3'>
+		<div onClick={() => setIsCliked(true)} className={cls('animate-bounce cursor-pointer absolute top-4 left-1/2 -translate-x-1/2  bg-white shadow-2xl rounded-2xl p-3 ', isClicked ? 'hidden pointer-events-none' : 'block')}>
 			<span className='text-sm text-searchResult'><strong className='text-primary'>{keyword}</strong> 검색결과</span>
 		</div>
 	)
