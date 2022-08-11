8bf0a71f31f187c3fe18a15bb8f898b24b1b2d0a
lee <ajaijs1103@naver.com>
2022-08-11 20:06:08 +0900

    feat: /api/user/logIn (로그인 api) 생성

---
+++ pages/api/user/logIn.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import client from '@libs/client'
+import { ResponseType } from '@utils/interfaces'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { password, email } = req.body
+  if (!password || !email) return res.status(400).json({ ok: false })
+  // 해당 email로 가입한 유저가 있는지 확인
+  const user = await client.user.findUnique({ where: { email } })
+  if (!user) return res.json({ ok: false, error: '계정이 존재하지 않습니다.' })
+  const isPwCorrect = await bcrypt.compare(password, user.password)
+  if (!isPwCorrect)
+    return res.json({ ok: false, error: '올바른 비밀번호가 아닙니다.' })
+  const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY as string)
+  res.json({
+    ok: true,
+    token,
+  })
+}
+
+export default handler
