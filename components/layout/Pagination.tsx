f708c2343c93918d0d244fe57159b27e875f1f6e
lee <ajaijs1103@naver.com>
2022-08-19 21:22:15 +0900

    refactor: 후기가 없을시 pagination 버튼 disabled

--- components/layout/Pagination.tsx
+++ components/layout/Pagination.tsx
@@ -4,14 +4,15 @@ interface Props {
 	limit: number
 	page: number
 	setPage: (page: number) => void
+	isEmpty: boolean
 }
-function Pagination({ limit, page, setPage }: Props) {
+function Pagination({ limit, page, setPage, isEmpty }: Props) {
 
 	return (
 		<nav className='' aria-label="Page navigation">
-			<ul className="w-full inline-flex justify-center items-center -space-x-px">
+			<ul className="inline-flex items-center justify-center w-full -space-x-px">
 				<li>
-					<button onClick={() => setPage(page - 1)} disabled={page === 1} className="block py-1 px-2 ml-0 leading-tight text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
+					<button onClick={() => setPage(page - 1)} disabled={page === 1 || isEmpty} className="block px-2 py-1 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700">
 						<span className="sr-only">Previous</span>
 						<svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
 					</button>
@@ -22,7 +23,7 @@ function Pagination({ limit, page, setPage }: Props) {
 					</li>
 				)}
 				<li>
-					<button onClick={() => setPage(page + 1)} disabled={page === limit} className="block py-1 px-2 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
+					<button onClick={() => setPage(page + 1)} disabled={page === limit || isEmpty} className="block px-2 py-1 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700">
 						<span className="sr-only">Next</span>
 						<svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
 					</button>
--- components/tabmenu/Comment.tsx
+++ components/tabmenu/Comment.tsx
@@ -65,9 +65,9 @@ function Review({ beachName }: Props) {
 		<div className='h-full p-2 overflow-y-auto' ref={reviewRef}>
 			<form onSubmit={handleSubmit(onValid)}>
 				<label htmlFor="review" className="sr-only">해수욕장 후기</label>
-				<div className="flex items-center py-1 px-3 bg-lightGray rounded-lg ">
-					<textarea disabled={!isLogin} {...register("payload", { required: true, pattern: { value: REVIEW_REGEX, message: FORM_ERR_MSG.invalidReview } })} id="review" rows={1} className="block mr-4 p-2 w-full text-xs text-gray-900 bg-white rounded-lg outline-none border-2 border-gray-300 focus:ring-primary focus:border-primary " placeholder="후기를 남겨주세요..."></textarea>
-					<button disabled={!isLogin || !formState.isValid} type="submit" className="disabled:opacity-10 inline-flex justify-center p-2 text-primary rounded-full cursor-pointer hover:bg-blue-100 ">
+				<div className="flex items-center px-3 py-1 rounded-lg bg-lightGray ">
+					<textarea disabled={!isLogin} {...register("payload", { required: true, pattern: { value: REVIEW_REGEX, message: FORM_ERR_MSG.invalidReview } })} id="review" rows={1} className="block w-full p-2 mr-4 text-xs text-gray-900 bg-white border-2 border-gray-300 rounded-lg outline-none focus:ring-primary focus:border-primary " placeholder="후기를 남겨주세요..."></textarea>
+					<button disabled={!isLogin || !formState.isValid} type="submit" className="inline-flex justify-center p-2 rounded-full cursor-pointer disabled:opacity-10 text-primary hover:bg-blue-100 ">
 						<svg aria-hidden="true" className="w-6 h-6 rotate-90" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path></svg>
 						<span className="sr-only">Send message</span>
 					</button>
@@ -75,12 +75,12 @@ function Review({ beachName }: Props) {
 				{formState.errors['payload']?.message && <p className="mt-2 text-xs text-red-600">{formState.errors['payload']?.message}</p>}
 
 			</form>
-			<div className='mt-4 space-y-1 relative'>
+			<div className='relative mt-4 space-y-1'>
 				{isLoading ? <Loader /> :
 					// isMyReview: 내가 작성한 후기만 삭제를 허용 하기 위해, 내 후기 여부를 나타내는 boolean 값 
 					(reviewData.total_cnt > 0) ? reviewData?.reviews?.map((review: any) => <Message isMyReview={profile?.id === review?.userId} reviewId={review?.id} key={review?.id} reviewDate={parseCreatedAt(review?.createdAt)} userName={review?.user?.name} payload={review?.payload} onReFetch={refetch} />) : <CommentNF />
 				}
-				<Pagination limit={pageLength} page={page} setPage={setPage} />
+				<Pagination isEmpty={reviewData.total_cnt === 0} limit={pageLength} page={page} setPage={setPage} />
 			</div>
 
 		</div>
